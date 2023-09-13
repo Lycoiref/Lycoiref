7ce427f178131246bb8728137c1738ec9e5f8ba2
Lycoiref <lycoiref@qq.com>
2023-09-13 19:05:23 +0800

    rebuild prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-    env: { browser: true, es2020: true, node: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'next/core-web-vitals',
-    ],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh', 'prettier'],
-    rules: {
-        'react-refresh/only-export-components': 'warn',
-        'prettier/prettier': 2,
-    },
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 2,
+  },
 }
--- src/app/pages/home/page.jsx
+++ src/app/pages/home/page.jsx
@@ -9,75 +9,73 @@ import React from 'react'
 import Typed from 'typed.js'
 
 const Test = styled.div`
-    color: red;
+  color: red;
 `
 
 const FirstPage = () => {
-    const myText = React.useRef(null)
+  const myText = React.useRef(null)
 
-    React.useEffect(() => {
-        const typed = new Typed(myText.current, {
-            strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-            typeSpeed: 50,
-        })
+  React.useEffect(() => {
+    const typed = new Typed(myText.current, {
+      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      typeSpeed: 50,
+    })
 
-        return () => {
-            // Destroy Typed instance during cleanup to stop animation
-            typed.destroy()
-        }
-    }, [])
-    console.log(myText)
-    return (
-        <div className="section">
-            <div className="section__content">
-                <span ref={myText} />
-            </div>
-        </div>
-    )
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy()
+    }
+  }, [])
+  console.log(myText)
+  return (
+    <div className="section">
+      <div className="section__content">
+        <span ref={myText} />
+      </div>
+    </div>
+  )
 }
 
 const Test2 = ({ fullpageApi }) => {
-    const [change, setChange] = React.useState(0)
-    console.log(fullpageApi)
-    return (
-        <div className="section">
-            <p>Section {change} (welcome to fullpage.js)</p>
-            <button onClick={() => setChange(change + 1)}>
-                Click me to add up
-            </button>
-        </div>
-    )
+  const [change, setChange] = React.useState(0)
+  console.log(fullpageApi)
+  return (
+    <div className="section">
+      <p>Section {change} (welcome to fullpage.js)</p>
+      <button onClick={() => setChange(change + 1)}>Click me to add up</button>
+    </div>
+  )
 }
 
 Test2.propTypes = {
-    fullpageApi: PropTypes.object,
+  fullpageApi: PropTypes.object,
 }
 
 function Home() {
-    return (
-        <>
-            <main>
-                <ReactFullpage
-                    //fullpage options
-                    licenseKey="gplv3-license"
-                    scrollingSpeed={600} /* Options here */
-                    render={({ fullpageApi }) => {
-                        return (
-                            <ReactFullpage.Wrapper>
-                                <FirstPage />
-                                <div className="section">
-                                    <p>Section 2</p>
-                                </div>
-                                <Test className="section">1111</Test>
-                                <Test2 fullpageApi={fullpageApi} />
-                                <Test className="section">1111</Test>
-                            </ReactFullpage.Wrapper>
-                        )
-                    }}
-                />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main>
+        <ReactFullpage
+          //fullpage options
+          licenseKey="gplv3-license"
+          scrollingSpeed={600} /* Options here */
+          render={({ fullpageApi }) => {
+            return (
+              <ReactFullpage.Wrapper>
+                <FirstPage />
+                <div className="section">
+                  <p>Section 2</p>
+                </div>
+                <Test className="section">1111</Test>
+                <Test2 fullpageApi={fullpageApi} />
+                <Test className="section">1111</Test>
+              </ReactFullpage.Wrapper>
+            )
+          }}
+        />
+      </main>
+    </>
+  )
 }
 
 export default Home
