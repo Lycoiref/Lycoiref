ca9387ee6ec7b4e516f20e3950fd7aacdcc9e525
Lycoiref <Lycoiref@users.noreply.github.com>
2023-11-23 06:54:05 +0000

    deploy: 39cc2c5e26a5447543ded403ec97b0e9f7b36440

---
+++ _next/static/chunks/app/pages/article/[slug]/page-6ef757aed7c17945.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[395],{7255:function(n,e,t){Promise.resolve().then(t.t.bind(t,3672,23)),Promise.resolve().then(t.t.bind(t,794,23)),Promise.resolve().then(t.t.bind(t,6363,23))},6363:function(){},794:function(){},3672:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=7255)}),_N_E=n.O()}]);
