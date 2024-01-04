f021c282300bf15b7cd77bbd837645eb6f49bf54
Lycoiref <lycoiref@qq.com>
2024-01-04 22:04:34 +0800

    -

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import styles from './page.module.scss'
 
 export default function Home() {
-  return <main className={styles.main}>Lycoiref的Blog，还在施工中......</main>
+  return (
+    <main className={styles.main}>
+      Lycoiref的Blog，还在施工中......
+      <div></div>
+    </main>
+  )
 }
